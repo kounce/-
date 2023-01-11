d345eae11696136b29a1a4978ac38f39606cec21
gazonomeshalka <118442125+gazonomeshalka@users.noreply.github.com>
2023-01-11 22:52:57 +0500

    Update test.py

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ def load_image(name, colorkey=None):
     image = pygame.image.load(fullname)
     if colorkey is not None:
         image = image.convert()
-        if colorkey == -1:
+        if colorkey == -1 or colorkey == '-1':
             colorkey = image.get_at((0, 0))
         image.set_colorkey(colorkey)
     else:
